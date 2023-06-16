d73375981e77f116f47e3d1b24a3d342682a8af0
riftriot <slushziesrazaxc@gmail.com>
2023-06-16 20:46:00 -0300

    Initial Commit

---
+++ index.cjs
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
