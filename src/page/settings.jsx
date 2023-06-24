b7d50307eae4f1c2fe8c5323e59bfa8fd1e8ea43
CompanionCubeGD <companioncubegd@gmail.com>
2023-06-24 17:57:48 -0400

    Added Basic Cloaking Options TODO: - Border Around Cloaking - Icon Cloaking - Localstorage Usage - Theme Swapping - Make TItle and Icon Cloaker Into a Form

--- src/page/settings.jsx
+++ src/page/settings.jsx
@@ -1,10 +1,25 @@
-import './style/style.css'
+import { title_cloak } from './scripts/settings';
+import { ab_cloak } from './scripts/settings.js';
+import './style/style.css';
 
 export default function Settings(props) {
     return (
         <div>
-            <div className="top-banner"></div>
-            SETTINGS
+    <div className="top-banner"></div>
+    <div className="container">
+        <div className="cloak">
+            <h1>Cloaking</h1>
         </div>
-    )
+            <div className="button-container">
+                <div className="ab">
+                    <button onClick={ab_cloak}>About:blank Cloaker</button>
+                </div>
+            <div className='title'>
+                <button onClick={title_cloak}>Title Cloaker</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+    );
 }
