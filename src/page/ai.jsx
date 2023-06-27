6bb96969629a4df2fe74873857b2e57ddb482186
Spritzerland <72568443+Spritzerland@users.noreply.github.com>
2023-06-26 19:05:13 -0700

    Update ai.jsx

--- src/page/ai.jsx
+++ src/page/ai.jsx
@@ -5,7 +5,18 @@ export default function AI(props) {
     return (
         <div>
             <div className="top-banner"></div>
-            AI
+            <div className="chat-history"></div>
+            <div className="main-content">
+                <div className="answer-box"></div>
+                <div className="message-box">
+                    <input></input>
+                    <a type="submit" className="send-img">
+                    <img src="/send-icon.png"></img>
+                    
+                    </a>
+                    
+                </div>
+            </div>
         </div>
     )
 }
