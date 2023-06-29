6b73d138b8e39e447ae768d3d616cfce75965b3b
Avad3 <_ssd@mailfence.com>
2023-06-29 17:04:04 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,26 @@ import node from "@astrojs/node"
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress({
+    exclude: [
+      (file) => file.includes("$server_build")
+    ],
+
+    logger: 0
+  })],
+  // adapter: cloudflare({
+  //   mode: "directory"
+  // }),
+  // adapter: node({
+  //   mode: "standalone"
+  // }), 
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Service-Worker-Allowed": "/",
+      "X-Content-Type-Options": "nosniff"
+    }
+  },
   output: "server"
 });
