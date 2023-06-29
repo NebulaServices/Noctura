a7969342c8d8abed31f5237cde65e5b13f563ea1
Avad3 <65318266+Avad3@users.noreply.github.com>
2023-06-28 23:37:36 -0400

    remove unneeded deps, fix dep structuere

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node"
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), compress({
+  integrations: [tailwind(), compress({
     exclude: [
       (file) => file.includes("$server_build")
     ],
 
     logger: 0
   })],
-  adapter: cloudflare({
-    mode: "directory"
-  }),
+  // adapter: cloudflare({
+  //   mode: "directory"
+  // }),
+  adapter: node({
+    mode: "standalone"
+  }), 
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
