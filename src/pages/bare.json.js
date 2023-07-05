e2c83669207593a65c78a541e50489c9deaea253
riftriot <slushziesrazaxc@gmail.com>
2023-07-04 21:16:38 -0300

    Revert "wack"
    This reverts commit 2f2f09ca659074081d743b9d6311db962bdb09ff.

--- src/pages/bare.json.js
+++ src/pages/bare.json.js
@@ -2,7 +2,6 @@ export async function get({params, request}) {
     return {
       body: JSON.stringify([
         'tomp.app',
-        '198.251.90.207',
         'us-east.noctura.tech',
         'us-west.noctura.tech',
         'eu-central.noctura.tech'
