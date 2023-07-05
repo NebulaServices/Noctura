2f2f09ca659074081d743b9d6311db962bdb09ff
genericness <107654710+genericness@users.noreply.github.com>
2023-07-05 00:03:54 +0000

    wack

--- src/pages/bare.json.js
+++ src/pages/bare.json.js
@@ -2,6 +2,7 @@ export async function get({params, request}) {
     return {
       body: JSON.stringify([
         'tomp.app',
+        '198.251.90.207',
         'us-east.noctura.tech',
         'us-west.noctura.tech',
         'eu-central.noctura.tech'
