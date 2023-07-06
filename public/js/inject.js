38c9648db821a90a609d27548b04c475376bd939
Ender <78042486+Sylvie-TN@users.noreply.github.com>
2023-07-05 20:54:19 -0400

    very basic menu

--- public/js/inject.js
+++ public/js/inject.js
@@ -1,1 +1,125 @@
-console.log('injected');
+const dom = new DOMParser().parseFromString('', 'text/html');
+
+var container = document.createElement('div');
+container.id = 'noctura-menu';
+
+container.innerHTML = `
+<button onclick="location.reload()">Reload</button>
+<button onclick="document.body.requestFullscreen()">Fullscreen</button>
+${window.parent !== window ? '<button onclick="">Close</button>' : ''}
+<button id="noctura-close-menu">Close Menu</button>
+
+<input id="noctura-nav-input" value="${new Function('return this.location.href')()}">
+`
+
+container.querySelector('#noctura-close-menu').onclick = function() {
+    container.style.display = 'none';
+}
+
+container.querySelector('#noctura-nav-input').onkeydown = function(e) {
+    if (e.key == 'Enter') {
+        console.log(this.value)
+        new Function(`return this.location.href = "${this.value}"`)();
+    }
+
+    return e;
+}
+
+var style = document.createElement('style');
+
+style.innerHTML = `
+#noctura-menu {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    height: min-content;
+    padding: 5px 10px;
+    border-radius: 5px;
+    background-color: #000000;
+    color: #ffffff;
+    transform: translate(-50%, 0);
+    z-index: 99999;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    font-family: sans-serif;
+    font-size: 12px;
+    font-weight: 600;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+}
+
+#noctura-nav-input {
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: #ffffff;
+    padding: 5px 10px;
+    border-radius: 5px;
+    transition: 0.1s ease-in-out;
+    font-family: sans-serif;
+    font-size: 12px;
+}
+
+#noctura-menu button {
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: #ffffff;
+    cursor: pointer;
+    padding: 5px 10px;
+    border-radius: 5px;
+    transition: 0.1s ease-in-out;
+}
+
+#noctura-menu button:hover {
+    background-color: #ffffff;
+    color: #000000;
+}
+`;
+
+container.appendChild(style);
+
+// make the container draggable
+
+var x1 = 0, y1 = 0, x2 = 0, y2 = 0, drag = false;
+
+container.addEventListener('mousedown', function(e) {
+    if (e.target.tagName == 'INPUT' || e.target.tagName == 'BUTTON') return;
+
+    e.preventDefault();
+
+    x2 = e.clientX;
+    y2 = e.clientY;
+
+    drag = true;
+});
+
+document.addEventListener('mousemove', function(e) {
+    if (drag) {
+        e.preventDefault();
+
+        x1 = x2 - e.clientX;
+        y1 = y2 - e.clientY;
+        x2 = e.clientX;
+        y2 = e.clientY;
+
+        if ((container.offsetTop - y1) < 0) return;
+        if ((container.offsetLeft - x1) < 0) return;
+
+        if ((container.offsetTop - y1) > window.innerHeight) return;
+        if ((container.offsetLeft - x1) > window.innerWidth) return;
+
+        container.style.top = (container.offsetTop - y1) + 'px';
+        container.style.left = (container.offsetLeft - x1) + 'px';
+    }
+});
+
+document.addEventListener('mouseup', function(e) {
+    drag = false;
+});
+
+document.body.appendChild(container);
--- public/sw/uv/uv.config.js
+++ public/sw/uv/uv.config.js
@@ -10,26 +10,32 @@ self.__uv$config = {
     sw: '/sw/uv/uv.sw.js',
 };
 
-if (typeof window == 'object') {
+if (typeof window == 'object' && (!window.parent.location.href.includes('/~/uv/') || window.top == window)) {
     fetch(location.origin + '/js/inject.js?sw=ignore').then(async (res) => {
         const text = await res.text();
         const script = document.createElement('script');
 
+        script.type = 'module';
         script.textContent = text;
 
         function load() {
             const shadow = document.createDocumentFragment();
             shadow.appendChild(script);
 
+            if (self.frameElement) self.frameElement.style.display = 'block';
+
             document.body.appendChild(shadow);
         }
 
-        if (document.readyState == 'interactive' || document.readyState == 'complete') {
+        if (document.readyState == 'complete' || document.readyState == 'interactive') {
+
             load();
         } else {
-        document.addEventListener('DOMContentLoaded', () => {
-            load();
-        });
+            document.addEventListener('readystatechange', () => {
+                if (document.readyState != 'complete' && document.readyState != 'interactive') return;
+
+                load();
+            });
         }
     });
 }
