8222e3bfa907d4bcbf83064bc5f8a1bfc15bc9fc
Ender <78042486+Sylvie-TN@users.noreply.github.com>
2023-07-10 20:45:02 -0400

    start settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import cloudflare from "@astrojs/cloudflare";
 import node from "@astrojs/node"
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), compress({
+  integrations: [tailwind(), react(), compress({
     exclude: [
       (file) => file.includes("$server_build")
     ],
--- src/pages/bare.json.js
+++ src/pages/bare.json.js
@@ -2,9 +2,6 @@ export async function get({params, request}) {
     return {
       body: JSON.stringify([
         'tomp.app',
-        'us-east.noctura.tech',
-        'us-west.noctura.tech',
-        'eu-central.noctura.tech'
       ]),
     };
   }
