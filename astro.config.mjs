e9fa1dbdd92e6938f99e51360ddb2480a1cc9a22
Avad3 <65318266+Avad3@users.noreply.github.com>
2023-07-15 03:02:16 -0400

    image enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import cloudflare from "@astrojs/cloudflare";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress({
     exclude: [file => file.includes("$server_build")],
     logger: 0
-  })],
+  }), astroImageTools],
   build: {  
     inlineStylesheets: 'auto'
   },
