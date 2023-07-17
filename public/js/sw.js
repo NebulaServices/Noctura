979bba2fb71d931bf63697bc34b7d3f53dd85347
Ender <78042486+Sylvie-TN@users.noreply.github.com>
2023-07-17 17:11:44 -0400

    make themes instantly load to prevent ugliness

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,9 +1,3 @@
-const theme = localStorage.getItem('theme') || 'amoled';
-document.documentElement.dataset.theme = theme;
-if (theme == "custom") {
-    document.querySelector('style#dynamic-style').innerHTML = `[data-theme="custom"] {${['primary-bg-color', 'secondary-bg-color', 'settings-1', 'settings-2', 'settings-3', 'font-color', 'primary-text-color', 'nav-btn-color-darker', 'nav-btn-color'].map(e=>localStorage.getItem(e) ? `--${e}: ${localStorage.getItem(e) + ' !important'}` : '').join(';')}}`;
-}
-
 if ('serviceWorker' in navigator) {
     window.registerSW = () => new Promise(e=>navigator.serviceWorker.getRegistrations().then(async function(registrations) {
         for await (let registration of registrations) {
