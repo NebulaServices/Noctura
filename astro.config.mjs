198e3a0cf8302fd55b671df1ba0744d5e0a52060
Avad3 <65318266+Avad3@users.noreply.github.com>
2023-07-18 06:25:27 -0400

    use esbuild for js/css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import { astroImageTools } from "astro-imagetools";
-import links from 'astro-link-preview';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
+import compression from './compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown({ config: { debug: false } }), sitemap(), compress({
+  integrations: [tailwind(), partytown({ config: { debug: false } }), sitemap(), compression(), compress({
     exclude: [file => file.includes("$server_build")],
-    logger: 0
+    logger: 0,
+    js: false,
+    css: false
   }), astroImageTools],
   site: 'https://noctura.tech',
   build: {  
---
+++ compress.js
@@ -0,0 +1,51 @@
+import { transform } from "esbuild";
+import { writeFile, readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { globby } from "globby";
+
+export default function compression() {
+    return {
+        name: "noctura-compress",
+        hooks: {
+            'astro:build:done': async({ dir }) => {
+                const files = await globby(`./dist/**/*.{css,js}`, {
+                    expandDirectories: true
+                });
+                files.map(async (file) => {
+                    console.log(file)
+                    const input = await readFile(file);
+                    let output;
+
+                    switch (true) {
+                        case file.endsWith("css"): {
+                            try {
+                                output = (await transform(input, {
+                                    minify: true,
+                                    minifyWhitespace: true,
+                                    loader: "css"
+                                })).code
+                            } catch (err) {
+                                console.log(err);
+                            }
+                        }
+
+                        case file.endsWith("js"): {
+                            try {
+                                output = (await transform(input, {
+                                    minify: true,
+                                    minifyWhitespace: true,
+                                    keepNames: true,
+                                    loader: "js"
+                                })).code
+                            } catch (err) {
+                                console.log(err);
+                            }
+                        }
+                    }
+
+                    await writeFile(file, output);
+                })
+            }
+        }
+    }
+}
