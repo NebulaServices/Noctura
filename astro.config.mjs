8cbb62f297d3d5bdd7e15e1920229b7da514348a
Ender <78042486+Sylvie-TN@users.noreply.github.com>
2023-07-18 13:19:00 -0400

    remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
-import node from "@astrojs/node";
+// import cloudflare from "@astrojs/cloudflare";
+// import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import { astroImageTools } from "astro-imagetools";
 import sitemap from '@astrojs/sitemap';
-import partytown from '@astrojs/partytown';
 import compression from './compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown({ config: { debug: false } }), sitemap(), compression(), compress({
+  integrations: [tailwind(), /*partytown({ config: { debug: false } }), */sitemap(), compression(), compress({
     exclude: [file => file.includes("$server_build")],
     logger: 0,
     js: false,
