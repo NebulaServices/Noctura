38a3564f04b00fdc9a35ba8b5756635541da4930
Avad3 <65318266+Avad3@users.noreply.github.com>
2023-07-22 18:33:36 -0400

    fix codemirror stuff

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,7 +1,11 @@
 import { EditorView, basicSetup } from "codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { javascript, javascriptLanguage, scopeCompletionSource } from "@codemirror/lang-javascript";
+// import proxyCompletions from "./proxyCompletions";
 import { css } from "@codemirror/lang-css";
+
+const scopedAutocomplete = javascriptLanguage.data.of({ autocomplete: scopeCompletionSource(globalThis) });
+// const proxyAutocomplete = javascriptLanguage.data.of({ autocomplete: proxyCompletions });
 
 function createEditor(lang) {
     if (lang === "css") {
@@ -11,11 +15,12 @@ function createEditor(lang) {
         });
     } else if (lang === "js") {
         return new EditorView({
-            extensions: [basicSetup, vscodeDark, javascript(), javascriptLanguage.data.of({ autocomplete: scopeCompletionSource(globalThis) })],
+            extensions: [basicSetup, vscodeDark, javascript(), scopedAutocomplete, /* proxyAutocomplete */],
             parent: document.querySelector("#monaco-editor"),
             // it has to look like that so it renders properly in the element
             doc:`/*
     Select an option above.
+    note: scripts that have no option will be ran in the current context when save is pressed
 */`
         });
     }
---
+++ src/js/proxyCompletions.js
@@ -0,0 +1,13 @@
+// don't delete this file or mess with the code
+// i'll set it up later
+/* export default function proxyCompletions(context) {
+    let word = context.matchBefore(/_{1,2}(uv|dynamic|aero)?\$?(config|location|meta)?/);
+    if (word.from == word.to && !context.explicit) return null
+
+    return {
+        from: word.from, 
+        options: [
+            { label: "__uv", type: "constant" }
+        ]
+    }
+} */
