d8324b7327350e18c4a1c49e7b2619b23b51bc43
rift <117926989+Riftriot@users.noreply.github.com>
2023-07-26 12:03:08 -0500

    add more nebula shit and consts file

---
+++ src/components/Consts.ts
@@ -0,0 +1,3 @@
+export const discord: string = "https://discord.gg/unblocker"
+export const docs: string = "https://docs.nebulaproxy.io"
+export const github: string = "https://github.com/NebulaServices"
